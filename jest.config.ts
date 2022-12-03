7405f6bc682846dc593a6f1bedc6623da736de15
Ratibus11 <49524093+Ratibus11@users.noreply.github.com>
2022-12-03 17:46:33 +0100

    Update jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -12,8 +12,8 @@ const jestConfig: JestConfigWithTsJest = {
     resetMocks: true,
     resetModules: true,
     modulePaths: [compilerOptions.baseUrl],
-    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
-    setupFiles: [path.resolve("tests/setup.ts")],
+    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths), // Will retrieve tsconfig.json's aliases and resolve it while running tests.
+    //setupFiles: [path.resolve("tests/setup.ts")], // This file allows to run specific script before running each test file.
 };
 
 export default jestConfig;
